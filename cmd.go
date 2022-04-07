bf937e1ebee43b2e93b1a5710f5a32e55ca081c4
Martin Bertschler <mbertschler@gmail.com>
2022-04-07 22:49:21 +0200

    index command

--- cmd.go
+++ cmd.go
@@ -2,6 +2,8 @@ package seal
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -19,6 +21,7 @@ func RootCmd() *cobra.Command {
 	}
 	cmd.AddCommand(sealCmd)
 	cmd.AddCommand(verifyCmd)
+	cmd.AddCommand(indexCmd)
 	return cmd
 }
 
@@ -33,13 +36,16 @@ func runSealCmd(cmd *cobra.Command, args []string) error {
 		return errors.New("need at least one path argument to seal")
 	}
 
+	start := time.Now()
 	for _, arg := range args {
 		PrintSealing = true
 		_, err := SealPath(arg)
 		if err != nil {
 			return errors.Wrap(err, "SealPath")
 		}
+
 	}
+	log.Println("ran for", time.Since(start))
 	return nil
 }
 
@@ -54,6 +60,7 @@ func runVerifyCmd(cmd *cobra.Command, args []string) error {
 		return errors.New("need at least one path argument to verify")
 	}
 
+	start := time.Now()
 	for _, arg := range args {
 		PrintVerify = true
 		print := true
@@ -61,6 +68,31 @@ func runVerifyCmd(cmd *cobra.Command, args []string) error {
 		if err != nil {
 			return errors.Wrap(err, "VerifyPath")
 		}
+	}
+	log.Println("ran for", time.Since(start))
+	return nil
+}
+
+var indexCmd = &cobra.Command{
+	Use:   "index",
+	Short: "indexes all directories and prints stats",
+	RunE:  runIndexCmd,
+}
+
+func runIndexCmd(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return errors.New("need at least one path argument to index")
+	}
+
+	PrintIndexProgress = true
+	for _, path := range args {
+		log.Println("indexing", path)
+		start := time.Now()
+		dirs, err := indexDirectories(path)
+		if err != nil {
+			return errors.Wrap(err, "indexDirectories")
+		}
+		log.Println("loaded", len(dirs), "directories in", time.Since(start))
 	}
 	return nil
 }
--- index.go
+++ index.go
@@ -3,11 +3,14 @@ package seal
 import (
 	"fmt"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"sort"
 	"strings"
+	"time"
 
+	"github.com/fatih/color"
 	"github.com/pkg/errors"
 )
 
@@ -20,6 +23,11 @@ type dir struct {
 	hash  *Diff
 }
 
+var (
+	PrintIndexProgress    = false
+	IndexProgressInterval = 15 * time.Second
+)
+
 // indexDirectories returns all subdirectories with info about their depth.
 // The deepest nested directories are sorted first.
 func indexDirectories(dirPath string) ([]*dir, error) {
@@ -31,15 +39,30 @@ func indexDirectories(dirPath string) ([]*dir, error) {
 		return nil, fmt.Errorf("%q is not a directory", dirPath)
 	}
 
+	var tick *time.Ticker
+	if PrintIndexProgress {
+		tick = time.NewTicker(IndexProgressInterval)
+		defer tick.Stop()
+	}
+
 	out := []*dir{}
 	err = filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			return err
+			log.Println(color.YellowString("can't index %q: %v", path, err))
+			return nil
 		}
 		if d.IsDir() {
 			path = filepath.Clean(path)
 			parts := strings.Split(path, "/")
 			out = append(out, &dir{path: path, depth: len(parts)})
+
+			if PrintIndexProgress {
+				select {
+				case <-tick.C:
+					log.Printf("indexing %q in depth %d", path, len(parts))
+				default:
+				}
+			}
 		}
 		return nil
 	})
